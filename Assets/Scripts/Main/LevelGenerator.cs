9a9e9ddba82010450ccad5c868c734eb99819fdc
Jusemon <juansmm@outlook.com>
2022-05-30 20:09:16 -0500

    Workaround to avoid StackOverflow on spiral

--- Assets/Scripts/Main/LevelGenerator.cs
+++ Assets/Scripts/Main/LevelGenerator.cs
@@ -10,13 +10,12 @@ public class LevelGenerator : MonoBehaviour
 
     [SerializeField]
     [Min(8)]
-    int numberOfRooms = 8;
+    int numberOfRooms = 9;
 
     [SerializeField]
     float distance = 10f;
 
     private List<GameObject> rooms;
-
 
     // Start is called before the first frame update
     void Start()
@@ -30,10 +29,10 @@ public class LevelGenerator : MonoBehaviour
         for (int index = 0; index < numberOfRooms; index++)
         {
             bool isInitial = false, isFinal = false;
-            Vector3 currentDirection;
+            Vector3? currentDirection;
             Vector3? previousDirection;
             var position = CalculatePosition(rooms, index, out currentDirection);
-            previousDirection = Direction.Reverse(currentDirection);
+            previousDirection = Direction.Reverse(currentDirection.GetValueOrDefault());
             // Initial level
             if (index == 0)
             {
@@ -42,10 +41,16 @@ public class LevelGenerator : MonoBehaviour
             }
             else
             {
-                UpdateRoom(index - 1, currentDirection);
+                if (!currentDirection.HasValue)
+                {
+                    UpdateRoom(index - 1, true);
+                    break;
+                }
+
+                UpdateRoom(index - 1, currentDirection.Value);
+
                 if (index == (numberOfRooms - 1))
                 {
-                    // UpdateRoom(index, currentDirection);
                     isFinal = true;
                 }
             }
@@ -53,34 +58,42 @@ public class LevelGenerator : MonoBehaviour
             var room = CreateRoom(position, previousDirection, isInitial, isFinal, index);
             previousDirection = currentDirection;
             rooms.Add(room);
+            if (isFinal)
+            {
+                UpdateRoom(index, true);
+            }
         }
     }
 
-    private Vector3 CalculatePosition(List<GameObject> map, int index, out Vector3 currentDirection)
+    private Vector3 CalculatePosition(List<GameObject> map, int index, out Vector3? currentDirection)
     {
         currentDirection = CalculateDirection(map, index);
         var position = transform.position;
-        if (index > 0)
+        if (index > 0 && currentDirection.HasValue)
         {
-            position = map[index - 1].transform.position + currentDirection * distance;
+            position = map[index - 1].transform.position + currentDirection.Value * distance;
         }
         return position;
     }
 
-    private Vector3 CalculateDirection(List<GameObject> map, int index)
+    private Vector3? CalculateDirection(List<GameObject> map, int index, int tries = 5)
     {
+        if (tries == 0)
+        {
+            return null;
+        }
+
         var currentDirection = Direction.GetRandom();
         if (index > 0)
         {
             var position = map[index - 1].transform.position + currentDirection * distance;
             if (map.Select(room => room.transform.position).Any(pos => pos == position))
             {
-                return CalculateDirection(map, index);
+                return CalculateDirection(map, index, --tries);
             }
         }
         return currentDirection;
     }
-
 
     private GameObject CreateRoom(Vector3 position, Vector3? prevDirection, bool isInitial, bool isFinal, int index)
     {
@@ -110,7 +123,17 @@ public class LevelGenerator : MonoBehaviour
         roomManager.ProcessRoom();
     }
 
-
+    private void UpdateRoom(int index, bool isFinal)
+    {
+        Debug.Log($"Updating Room {index + 1}");
+        var room = rooms[index];
+        var roomManager = room.GetComponent<RoomManager>();
+        Debug.Log($"  With: ");
+        Debug.Log($"    isFinal: {isFinal}");
+        roomManager.isFinal = isFinal;
+        Debug.Log($"Proccesing Room {index + 1}");
+        roomManager.ProcessRoom();
+    }
 
     // Update is called once per frame
     void Update()
